258834bf2a6fccc9e97f25a677905348fba17d21
Christoph Rüßler <christoph.ruessler@mailbox.org>
2026-01-10 19:23:40 +0100

    Add a test combining renames and edits on multiple branches

--- gix-blame/tests/fixtures/make_blame_rename_tracking_repo.sh
+++ gix-blame/tests/fixtures/make_blame_rename_tracking_repo.sh
@@ -35,3 +35,33 @@ git checkout main
 git merge different-branch || true
 
 git blame --porcelain after-rename.txt > .git/after-rename.baseline
+
+echo -e "line 1\nline 2\nline 3\nline 4\nline 5\n" > change-and-rename.txt
+git add change-and-rename.txt
+git commit -q -m c2.1.1
+
+echo -e "line 1\nline 2 changed\nline 3\nline 4\nline 5\n" > change-and-rename.txt
+git add change-and-rename.txt
+git commit -q -m c2.1.2
+
+git checkout -b branch-that-renames-file
+git reset --hard HEAD~1
+
+echo -e "line 1\nline 2\nline 3\nline 4 changed\nline 5\n" > change-and-rename.txt
+git add change-and-rename.txt
+git commit -q -m c2.2.1
+
+mv change-and-rename.txt change-and-renamed.txt
+git add change-and-rename.txt change-and-renamed.txt
+git commit -q -m c2.2.2
+
+echo -e "line 1\nline 2\nline 3\nline 4 changed\nline 5 changed\n" > change-and-renamed.txt
+git add change-and-renamed.txt
+git commit -q -m c2.2.2
+
+git checkout main
+git merge branch-that-renames-file || true
+git add change-and-rename.txt
+git commit --no-edit
+
+git blame --porcelain change-and-renamed.txt > .git/change-and-renamed.baseline
