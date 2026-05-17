abf09b72735dea4244db4bbae5cda5dfb50d94c8
SarthakB11 <sarthak.bhardwaj21b@iiitg.ac.in>
2026-05-17 19:26:35 +0000

    gix-url: fix fuzz target after parse() signature change
    `parse(impl Into<&'a BStr>)` makes `data.into()` ambiguous; use
    `data.as_bstr()` via `bstr::ByteSlice` for an unambiguous `&BStr`.

--- gix-url/fuzz/fuzz_targets/parse.rs
+++ gix-url/fuzz/fuzz_targets/parse.rs
@@ -1,11 +1,12 @@
 #![no_main]
 use anyhow::Result;
+use bstr::ByteSlice;
 use libfuzzer_sys::fuzz_target;
 use std::hint::black_box;
 use std::path::Path;
 
 fn fuzz(data: &[u8]) -> Result<()> {
-    let url = gix_url::parse(data.into())?;
+    let url = gix_url::parse(data.as_bstr())?;
     _ = black_box(url.user());
     _ = black_box(url.password());
     _ = black_box(url.password());
@@ -20,7 +21,7 @@ fn fuzz(data: &[u8]) -> Result<()> {
     _ = black_box(url.canonicalized(Path::new("/cwd")));
     _ = black_box(url.to_bstring());
 
-    _ = black_box(gix_url::expand_path::parse(data.into()));
+    _ = black_box(gix_url::expand_path::parse(data.as_bstr()));
     Ok(())
 }
 
