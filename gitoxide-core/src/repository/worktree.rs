4ab146012d0af1c27b2352a78fcc2bb65d5c870c
shvvkz <viggocasciano0301@gmail.com>
2026-05-21 19:56:57 +0200

    fix: use main repo when listing worktrees

--- gitoxide-core/src/repository/worktree.rs
+++ gitoxide-core/src/repository/worktree.rs
@@ -6,24 +6,58 @@ pub fn list(repo: gix::Repository, out: &mut dyn std::io::Write, format: OutputF
     if format != OutputFormat::Human {
         bail!("JSON output isn't implemented yet");
     }
+    let main_repo = repo.main_repo()?;
+    let mut worktrees = Vec::new();
 
-    if let Some(worktree) = repo.worktree() {
-        writeln!(
-            out,
-            "{base} [{branch}]",
-            base = gix::path::realpath(worktree.base())?.display(),
-            branch = repo
-                .head_name()?
-                .map_or("<detached>".into(), |name| name.shorten().to_owned()),
-        )?;
+    if let Some(worktree) = main_repo.worktree() {
+        worktrees.push(create_worktree_info(&main_repo, gix::path::realpath(worktree.base())?)?);
     }
-    for proxy in repo.worktrees()? {
-        writeln!(
-            out,
-            "{base} [{name}]",
-            base = proxy.base()?.display(),
-            name = proxy.id()
-        )?;
+
+    for proxy in main_repo.worktrees()? {
+        let base = proxy.base()?;
+        let worktree_repo = proxy.into_repo()?;
+
+        worktrees.push(create_worktree_info(&worktree_repo, base)?);
     }
+
+    let path_width = worktrees.iter().map(|worktree| worktree.base.len()).max().unwrap_or(0);
+
+    for worktree in worktrees {
+        worktree.write(out, path_width)?;
+    }
+
     Ok(())
+}
+
+struct WorktreeInfo {
+    base: String,
+    head: String,
+    branch: String,
+}
+
+impl WorktreeInfo {
+    fn write(&self, out: &mut dyn std::io::Write, path_width: usize) -> std::io::Result<()> {
+        writeln!(
+            out,
+            "{:<path_width$} {} [{}]",
+            self.base,
+            self.head,
+            self.branch,
+            path_width = path_width,
+        )
+    }
+}
+
+fn create_worktree_info(repo: &gix::Repository, base: std::path::PathBuf) -> anyhow::Result<WorktreeInfo> {
+    let head = repo.head_id()?.to_hex_with_len(9).to_string();
+    let branch = repo
+        .head_name()?
+        .map_or("<detached>".into(), |name| name.shorten().to_owned())
+        .to_string();
+
+    Ok(WorktreeInfo {
+        base: base.display().to_string(),
+        head,
+        branch,
+    })
 }
